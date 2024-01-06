d6e4efa8ee3f3a42a67dbf59e27f298af45b9d27
AmirHosein Rahmati <amirhossein@smartech.ir>
2024-01-06 15:26:41 +0330

    broker_first_structure

---
+++ src/test/java/mq/project/broker/BrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package mq.project.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
