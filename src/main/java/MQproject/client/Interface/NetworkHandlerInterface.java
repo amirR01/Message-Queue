224ac7b067c6d9616e69c8ebc2ccfea608a1d314
alienit <ali.blade.1380@gmail.com>
2024-02-04 15:13:32 -0700

    Fixed readMessage, sendMessage in network handler.

--- src/main/java/MQproject/client/Interface/NetworkHandlerInterface.java
+++ src/main/java/MQproject/client/Interface/NetworkHandlerInterface.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public interface NetworkHandlerInterface {
     int connect(String ip, int port);
     void disconnect(String ip, int port);
-    String readMessage(int localPort);
-    void sendMessage(int localPort, String message);
+    String readMessage(String ip, int port);
+    void sendMessage(String ip, int port, String message);
 }
