0fdf26cbbd5650768e244a1a914f7722ed42219e
alienit <ali.blade.1380@gmail.com>
2024-02-09 02:12:39 -0700

    load balancer interface implemented.

---
+++ src/main/java/MQproject/server/Implementation/BrokerLoadBalancerImpl.java
@@ -0,0 +1,8 @@
+package MQproject.server.Implementation;
+
+
+// TODO: broker is lazier than server -> server should notify effected brokers
+
+public class BrokerLoadBalancerImpl {
+
+}
---
+++ src/main/java/MQproject/server/Interface/BrokerLoadBalancer.java
@@ -0,0 +1,10 @@
+package MQproject.server.Interface;
+
+// Situations:
+// 1. Broker registers
+// 2. Broker dies
+// 3. New partition gets added.
+// any others?
+
+public interface BrokerLoadBalancer {
+}
