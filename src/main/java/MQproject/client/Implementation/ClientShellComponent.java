53f4d72ee8f0e90c5d67fd6c7e2700a9e31a5588
freakyfaces <70147234+freakyfaces@users.noreply.github.com>
2024-02-11 23:46:00 +0330

    added shell commands

--- src/main/java/MQproject/client/Implementation/ClientShellComponent.java
+++ src/main/java/MQproject/client/Implementation/ClientShellComponent.java
@@ -3,6 +3,7 @@ package MQproject.client.Implementation;
 import MQproject.client.Interface.Consumer;
 import MQproject.client.Interface.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
@@ -15,12 +16,35 @@ public class ClientShellComponent {
     @Autowired
     public Consumer consumer;
 
+    @Value("${MQproject.client.consumer}")
+    public Boolean isConsumer;
+
+    @Value("${MQproject.client.producer}")
+    public Boolean isProducer;
+
+
     @ShellMethod(key = "produce")
     public void produce(@ShellOption String key,
                         @ShellOption String value) {
         System.out.println(key);
         System.out.println(value);
-        producer.produceMessage(key, value);
+        if (isProducer){
+            producer.produceMessage(key, value);
+        }
+    }
+
+    @ShellMethod(key = "pull")
+    public void pull() {
+        if (isConsumer){
+            consumer.pull();
+        }
+    }
+
+    @ShellMethod(key = "subscribe")
+    public void subscribe() {
+        if (isConsumer){
+            consumer.subscribe();
+        }
     }
 
 }
