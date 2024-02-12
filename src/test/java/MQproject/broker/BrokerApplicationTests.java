736135161b132820b5aa4550dd08f0dd40006c82
AmirHosein Rahmati <amirhossein@smartech.ir>
2024-02-12 23:04:58 +0330

    Revert "Added Tests for Broker"
    This reverts commit d44e7dfbd50ec97f1ab3ac62d82577fb75b38c43.

--- src/test/java/MQproject/broker/BrokerApplicationTests.java
+++ src/test/java/MQproject/broker/BrokerApplicationTests.java
@@ -1,10 +1,6 @@
 package MQproject.broker;
 
-import MQproject.broker.Implementation.BrokerServerServiceImpl;
 import MQproject.broker.Interface.BrokerServerService;
-import MQproject.broker.model.message.BrokerServerMessageAboutBrokers;
-import MQproject.broker.model.message.MessageType;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,95 +8,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-
-import java.util.ArrayList;
 import java.util.HashMap;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-
+import java.util.List;
 
 @SpringBootTest
 class BrokerApplicationTests {
-
-	private BrokerServerServiceImpl brokerServerServiceImpl ;
-
-	public HashMap<Integer, ArrayList<Integer>> brokerIdToLeaderPartitionsTest = new HashMap<>();
-	public HashMap<Integer, ArrayList<Integer>> brokerIdToReplicaPartitions = new HashMap<>();
-	public HashMap<Integer, ArrayList<Integer>> consumerIdToPartitionsTest = new HashMap<>();
-
-
-
-	@BeforeEach
-    void setUp() {
-        brokerServerServiceImpl = new BrokerServerServiceImpl();
-    }
-
-	@Test
-	void testLoadBalancerOnConsumerBirth() {
-		int brokerId1 = 1;
-		
-		// ArrayList<Integer> leaderPartitionsTest = new ArrayList<>(Arrays.asList(1, 2));
-		// ArrayList<Integer> replicaPartitionsTest = new ArrayList<>(Arrays.asList(3, 4));
-		// brokerIdToLeaderPartitionsTest.put(brokerId1, leaderPartitionsTest);
-		// brokerIdToReplicaPartitions.put(brokerId1, replicaPartitionsTest);
-	
-		// // Invoke the method under test and assert the result
-		// Assertions.assertThrows(NullPointerException.class, () -> {
-		// 	consumerLoadBalancerImpl.balanceOnConsumerBirth(consumerIdToPartitionsTest, replicaPartitionsTest, null);
-		// });
-		Assertions.assertDoesNotThrow(() -> brokerServerServiceImpl.getBrokerAddress(brokerId1));
-
-	}
+	@Autowired
+	BrokerServerService brokerServerService;
+	private final RestTemplate restTemplate = new RestTemplate();
 
 
 	@Test
-	void testGetBrokerId() {
-		Assertions.assertDoesNotThrow(() -> brokerServerServiceImpl.getMyBrokerId());
+	void contextLoads() {
+
 	}
 
 	@Test
-	void testRegisterToServer() {
-		int brokerId1 = 1;
-		
-		// ArrayList<Integer> leaderPartitionsTest = new ArrayList<>(Arrays.asList(1, 2));
-		// ArrayList<Integer> replicaPartitionsTest = new ArrayList<>(Arrays.asList(3, 4));
-		// brokerIdToLeaderPartitionsTest.put(brokerId1, leaderPartitionsTest);
-		// brokerIdToReplicaPartitions.put(brokerId1, replicaPartitionsTest);
-	
-		// // Invoke the method under test and assert the result
-		// Assertions.assertThrows(NullPointerException.class, () -> {
-		// 	brokerLoadBalancerImpl.balanceOnBrokerBirth(brokerIdToLeaderPartitionsTest, brokerIdToReplicaPartitions, null);
-		// });
-
-		// myBrokerId
-
-		// BrokerServerMessageAboutBrokers bigMessage = new BrokerServerMessageAboutBrokers();
-        // bigMessage.messages.add(
-        //         new BrokerServerMessageAboutBrokers.BrokerServerSmallerMessageAboutBrokers(
-        //                 myBrokerId, myIp, myPort, MessageType.REGISTER_BROKER
-        //         )
-        // );
-
-		// Assertions.assertDoesNotThrow(() -> brokerServerServiceImpl.registerToServer());
-
+	void testProduceMessage(){
+//		Message = sadsadsd;
+//		String awnser = brokerServerService.produceMessage();
+//		ResponseEntity restTemplate.postForEntity(
+//
+//		)
+//		Assert.assertEquals();
 	}
+	//     broker id , partition ids
+	HashMap<Integer, List<Integer>> partitionsLeaders = new HashMap<>();
+	HashMap<Integer, List<Integer>> partitionsReplicas = new HashMap<>();
 
-	// @Test
-	// void testLoadBalancerOnBrokerDeath() {
-	// 	int brokerId1 = 1;
-		
-	// 	ArrayList<Integer> leaderPartitionsTest = new ArrayList<>(Arrays.asList(1, 2));
-	// 	ArrayList<Integer> replicaPartitionsTest = new ArrayList<>(Arrays.asList(3, 4));
-	// 	brokerIdToLeaderPartitionsTest.put(brokerId1, leaderPartitionsTest);
-	// 	brokerIdToReplicaPartitions.put(brokerId1, replicaPartitionsTest);
-	
-	// 	// Invoke the method under test and assert the result
-	// 	Assertions.assertThrows(NullPointerException.class, () -> {
-	// 		brokerLoadBalancerImpl.balanceOnBrokerDeath(brokerIdToLeaderPartitionsTest, brokerIdToReplicaPartitions, brokerId1);
-	// 	});
-	// }
+
 
 }
-
-
